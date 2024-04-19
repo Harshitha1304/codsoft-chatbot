8e0b93e9c92e75ca97c9d1f2ccdc0738cd6cf575
Harshitha1304 <155815153+Harshitha1304@users.noreply.github.com>
2024-04-19 16:05:05 +0530

    Add files via upload

---
+++ recommendation system.py
@@ -0,0 +1,212 @@
+# Data for books and songs
+books_data = {
+  "K&R C Book,The Complete Reference,Expert C Programming": {"author": "Dennis Ritchie, Herbert Schildt, Peter Van Der Linden", "Language": "C Language"},
+  "Stuff with Python,Fluent Python,Learn Python the Hard Way": {"author": "Al Sweigart, Luciano Ramalho, Zed A. Shaw", "Language": "Python"},
+  "Head First Java,Effective Java,Thinking in Java": {"author": "Kathy Sierra, Joshua Bloch, Bruce Eckel", "Language": "Java"},
+  "C++ Programming Lag,C++ Primer,Effective Modern C++": {"author": "Bjarne Stroustrup, Stanley B. Lippman, Scott Meyers", "Language": "C++"},
+  "SQL All in One,Learning SQL,Head First SQL": {"author": "Rick Morelan, Alan Beaulieu, Lynn Beighley", "Language": "SQL"},
+  "Introduction to Algorithms,Algorithm Design Manual,Algorithms": {"author": "Thomas H. Cormen, Steven S. Skiena, Robert Sedgewick", "Language": "Algorithm"},
+  "JavaScript: The Good Parts,Eloquent JavaScript,You Don't Know JS": {"author": "Douglas Crockford, Marijn Haverbeke, Kyle Simpson", "Language": "JavaScript"},
+  "Ruby on Rails Tutorial,The Well-Grounded Rubyist,Programming Ruby": {"author": "Michael Hartl, David A. Black, Dave Thomas", "Language": "Ruby"},
+  "Learning PHP,PHP Objects, Patterns, and Practice,Modern PHP": {"author": "David Sklar, Matt Zandstra, Josh Lockhart", "Language": "PHP"},
+  "Python Crash Course,Automate the Boring Stuff with Python,Python Cookbook": {"author": "Eric Matthes, Al Sweigart, David Beazley", "Language": "Python"},
+  "Clean Code,Code Complete,The Pragmatic Programmer": {"author": "Robert C. Martin, Steve McConnell, Andrew Hunt", "Language": "General Programming"},
+  "Python for Data Analysis,Data Science from Scratch,Hands-On Machine Learning with Scikit-Learn": {"author": "Wes McKinney, Joel Grus, Aurélien Géron", "Language": "Python"},
+  "Learning Perl,Intermediate Perl,Mastering Perl": {"author": "Randal L. Schwartz, Brian D Foy, brian d foy", "Language": "Perl"},
+  "Learning Ruby,The Ruby Way,Agile Web Development with Rails": {"author": "Michael Hartl, Hal Fulton, Sam Ruby", "Language": "Ruby"},
+  "Node.js in Action,Professional Node.js,Mastering Node.js": {"author": "Mike Cantelon, Pedro Teixeira, TJ Holowaychuk", "Language": "Node.js"},
+  "Learning SQL,SQL Antipatterns,SQL Cookbook": {"author": "Alan Beaulieu, Bill Karwin, Anthony Molinaro", "Language": "SQL"},
+  "Effective Python,Fluent Python,Python Tricks": {"author": "Brett Slatkin, Luciano Ramalho, Dan Bader", "Language": "Python"},
+  "Java Concurrency in Practice,Java Performance,Java 8 in Action": {"author": "Brian Goetz, Charlie Hunt, Raoul-Gabriel Urma", "Language": "Java"},
+  "Beginning C++,Accelerated C++,C++ Primer Plus": {"author": "Ivor Horton, Andrew Koenig, Stephen Prata", "Language": "C++"},
+  "Learning Java,OCA/OCP Java SE 8 Programmer,Head First Design Patterns": {"author": "Kathy Sierra, Jeanne Boyarsky, Eric Freeman", "Language": "Java"},
+  "Scala for the Impatient,Programming in Scala,Scala Cookbook": {"author": "Cay Horstmann, Martin Odersky, Alvin Alexander", "Language": "Scala"},
+  "Learning Perl,Intermediate Perl,Mastering Perl": {"author": "Randal L. Schwartz, brian d foy, brian d foy", "Language": "Perl"},
+  "JavaScript: The Definitive Guide,You Don't Know JS,JavaScript Patterns": {"author": "David Flanagan, Kyle Simpson, Stoyan Stefanov", "Language": "JavaScript"},
+  "Learning Python,Python Crash Course,Fluent Python": {"author": "Mark Lutz, Eric Matthes, Luciano Ramalho", "Language": "Python"},
+  "Java Generics and Collections,Head First Java,Java EE 7 Essentials": {"author": "Maurice Naftalin, Kathy Sierra, Arun Gupta", "Language": "Java"},
+  "C# in Depth,Pro C# 7,Essential C# 7.0": {"author": "Jon Skeet, Andrew Troelsen, Mark Michaelis", "Language": "C#"},
+  "R for Data Science,Advanced R,Machine Learning with R": {"author": "Hadley Wickham, Hadley Wickham, Brett Lantz", "Language": "R"},
+  "Learning Swift,Swift Programming,Swift in Depth": {"author": "Paris Buttfield-Addison, Jon Manning, Tjeerd in 't Veen", "Language": "Swift"},
+  "HTML and CSS: Design and Build Websites,JavaScript and JQuery,Web Design with HTML, CSS, JavaScript and jQuery Set": {"author": "Jon Duckett, Jon Duckett, Jon Duckett", "Language": "HTML/CSS/JavaScript"},
+  "Learning React Native,React Native Cookbook,Mastering React Native": {"author": "Bonnie Eisenman, William M. Peña, Eric Masiello", "Language": "React Native"},
+  "Learning SQL,SQL Antipatterns,SQL Cookbook": {"author": "Alan Beaulieu, Bill Karwin, Anthony Molinaro", "Language": "SQL"},
+  "Programming Ruby,The Ruby Way,Awesome Ruby": {"author": "Dave Thomas, Hal Fulton, Hal Fulton", "Language": "Ruby"},
+  "JavaScript and JQuery,JavaScript: The Good Parts,Head First JavaScript Programming": {"author": "Jon Duckett, Douglas Crockford, Eric T. Freeman", "Language": "JavaScript"},
+  "The C Programming Language,C Primer Plus,Expert C Programming": {"author": "Brian W. Kernighan, Stephen Prata, Peter Van Der Linden", "Language": "C Language"},
+  "Python Pocket Reference,Python Cookbook,Python Tricks": {"author": "Mark Lutz, David Beazley, Dan Bader", "Language": "Python"},
+  "Java How To Program,Head First Java,Java: A Beginner's Guide": {"author": "Paul Deitel, Kathy Sierra, Herbert Schildt", "Language": "Java"},
+  "C++ Primer Plus,Accelerated C++,C++ Templates": {"author": "Stephen Prata, Andrew Koenig, David Vandevoorde", "Language": "C++"}
+}
+
+songs_data = {
+    "Maguva Maguva": {"artist": "Sid Sriram", "movie": "Vakeel Sab"},
+    "Baguntundi nuvvu": {"artist": "Sid Sriram", "movie": "Atithi Devo Bhava"},
+    "Nandanandanaa": {"artist": "Sid Sriram", "movie": "FamlilyStar"},
+    "Maate Vinaduga": {"artist": "Sid Sriram", "movie": "Taxiwala"},
+    "Urike Urike": {"artist": "Sid Sriram", "movie": "Hit2"},
+    "Manasa Manasa": {"artist": "Sid Sriram", "movie": "Most Eligible Bachelor"},
+    "Kalalo Kooda": {"artist": "Sid Sriram", "movie": "Liger"},
+    "Mellaga": {"artist": "Chithra", "movie": "Varsham"},
+    "Anthaistam": {"artist": "Chithra", "movie": "Bhemla Nayak"},
+    "Gudilo Badilo Madilo": {"artist": "Chithra", "movie": "DJ"},
+    "Kannalane": {"artist": "Chithra", "movie": "Bombay"},
+    "Nee Choopule": {"artist": "Chithra", "movie": "Endukunthe Prematha"},
+    "Muvvala Navakala": {"artist": "Chithra", "movie": "Pournamy"},
+    "O rendu prema meghaalila": {"artist": "Sreerama Chandra", "movie": "Baby"},
+    "Atu itu ooguthu": {"artist": "Sreerama Chandra", "movie": "Life is beautiful"},
+    "Beast Mode": {"artist": "Sreerama Chandra", "movie": "Beast"},
+    "Not Ramaiya vastavaiya": {"artist": "Sreerama Chandra", "movie": "Jawan"},
+    "Gelupu Thalupule": {"artist": "Sreerama Chandra", "movie": "TeenMar"},
+    "Jala Jala Jalapaatham": {"artist": "shreya ghoshal", "movie": "Uppena"},
+    "Poolane Kunukeyamant": {"artist": "shreya ghoshal", "movie": "I"},
+    "Chali Chaliga": {"artist": "shreya ghoshal", "movie": "Mr. Prefect"},
+    "Vastunna Vachestunna": {"artist": "shreya ghoshal", "movie": "V"},
+    "Allasani Vaari": {"artist": "shreya ghoshal", "movie": "Tholiprema"},
+    "Emo Emo": {"artist": "shreya ghoshal", "movie": "Katamarayadhu"},
+    "Blockbuster": {"artist": "shreya ghoshal", "movie": "Saarinoodhu"},
+    "Nee Jathaga": {"artist": "shreya ghoshal", "movie": "Yevadhu"},
+    "Humma Humma": {"artist": "ram miriyala", "movie": "Ooru Peru Bhairavakona"},
+    "Potti Pilla": {"artist": "ram miriyala", "movie": "Balagam"},
+    "Ticket Eh Konakunda": {"artist": "ram miriyala", "movie": "Tillu 2"},
+    "Chamkeela Angeelesi": {"artist": "ram miriyala", "movie": "Dasara"},
+    "Chitti": {"artist": "ram miriyala", "movie": "Jathiratnalu"},
+    "Radhika": {"artist": "ram miriyala", "movie": "Tillu 2"},
+    "Jorse Jorse": {"artist": "Geetha Madhuri", "movie": "magadheera"},
+    "Top lesi poddi": {"artist": "Geetha Madhuri", "movie": "Idharammayilatho"},
+    "Darlingey": {"artist": "Geetha Madhuri", "movie": "Mirchi"},
+    "Deo Deo": {"artist": "Geetha Madhuri", "movie": "Garudavega"},
+    "Chedu Nijam": {"artist": "Geetha Madhuri", "movie": "Hi Nanna"},
+    "Nuvvem Maya": {"artist": "Geetha Madhuri", "movie": "Okkadu"},
+    "Dilaku Dilaku": {"artist": "Geetha Madhuri", "movie": "Racha"},
+    "Kala Kala Kalamandhir": {"artist": "Geetha Madhuri", "movie": "Intelligent"},
+    "Anuvanuvuu": {"artist": "Arjith Singh", "movie": "Om bheem bush"},
+}
+
+movie_data={
+  "Bahubali":{"Director":"S.S Rajamouli ","genre":"All genres"},
+"Eega":{"Director":"S.S Rajamouli ","genre":"All genres"},
+"Marayadaramana":{"Director":"S.S Rajamouli ","genre":"All genres"},
+"Chhatrapati":{"Director":"S.S Rajamouli ","genre":"All genres"},
+"Vikramarkudu":{"Director":"S.S Rajamouli ","genre":"All genres"},
+"Gunturukaaram":{"Director":"Trivikram Srinivas","genre":"All genres"},
+"Ala Vaikuntapurramu":{"Director":"Trivikram Srinivas","genre":"All genres"},
+"Aravindha Sametha Verra Raghava":{"Director":"Trivikram Srinivas","genre":"All genres"},
+"Athadu":{"Director":"Trivikram Srinivas","genre":"All genres"},
+"Pokiri":{"Director":"Puri Jagannadh ","genre":"All genres"},
+"Bujjigadu":{"Director":"Puri Jagannadh ","genre":"All genres"},
+"Temper":{"Director":"Puri Jagannadh ","genre":"All genres"},
+"Badri":{"Director":"Puri Jagannadh ","genre":"All genres"},
+"Loafer":{"Director":"Puri Jagannadh ","genre":"All genres"},
+"I":{"Director":"S.Shankar ","genre":"All genres"},
+"Robo,Robo":{"Director":"S.Shankar ","genre":"All genres"},
+"2.O":{"Director":"S.Shankar ","genre":"All genres"},
+"Aparichithudu":{"Director":"S.Shankar","genre":"All genres"},
+"Jeans":{"Director":"S.Shankar ","genre":"All genres"},
+"Srimanthudu":{"Director": "Koratala Siva ", "genre": "All genres"},
+"Bharat Ane Nenu": {"Director": "Koratala Siva ", "genre": "All genres"},
+"Nenokkadine": {"Director": "Koratala Siva ", "genre": "All genres"},
+"Dookudu": {"Director": "Koratala Siva ", "genre": "All genres"},
+"Businessman": {"Director": "Koratala Siva ", "genre": "All genres"},
+"Manam": {"Director": "Vikram K. Kumar", "genre": "All genres"},
+"Hello": {"Director": "Vikram K. Kumar", "genre": "All genres"},
+"Majnu":  {"Director": "Vikram K. Kumar", "genre": "All genres"},
+"Oopiri": {"Director": "Vikram K. Kumar", "genre": "All genres"}, 
+"Soggade Chinni Nayana": {"Director": "Vikram K. Kumar", "genre": "All genres"},
+"Arjun Reddy":{"Director": "Sandeep Reddy Vanga ", "genre": "All genres"},
+"Geetha Govindam":{"Director": "Sandeep Reddy Vanga ", "genre": "All genres"},
+"Dear Comrade":{"Director": "Sandeep Reddy Vanga ", "genre": "All genres"},
+"World Famous Lover":{"Director": "Sandeep Reddy Vanga ", "genre": "All genres"},
+"Aa Naluguru":{"Director": "Ram Gopal Varma ", "genre": "All genres"},
+"Dongata":{"Director": "Ram Gopal Varma ", "genre": "All genres"},
+"Gaayam":{"Director": "Ram Gopal Varma ", "genre": "All genres"},
+"Gaayam 2":{"Director": "Ram Gopal Varma ", "genre": "All genres"},
+"Money":{"Director": "Ram Gopal Varma ", "genre": "All genres"},
+"The Ghazi Attack": {"Director": "Sankalp Reddy", "genre": "All genres"},
+"Anthariksham 9000 KMPH": {"Director": "Sankalp Reddy", "genre": "All genres"},
+"Sarrainodu": {"Director": "Surender Reddy", "genre": "All genres"},
+"Race Gurram": {"Director": "Surender Reddy", "genre": "All genres"},
+"Arya": {"Director": "Surender Reddy", "genre": "All genres"},
+"Arya 2": {"Director": "Surender Reddy", "genre": "All genres"},
+"Julayi": {"Director": "Surender Reddy", "genre": "All genres"},
+"Ee Nagaraniki Emaindi": {"Director": "Tharun Bhascker Dhaassya", "genre": "All genres"},
+"Brochevarevarura": {"Director": "Tharun Bhascker Dhaassya", "genre": "All genres"},
+"Ami Thumi": {"Director": "Tharun Bhascker Dhaassya", "genre": "All genres"},
+"Pelli Choopulu": {"Director": "Tharun Bhascker Dhaassya", "genre": "All genres"},
+"Mental Madhilo": {"Director": "Tharun Bhascker Dhaassya", "genre": "All genres"},
+"Bhale Bhale Magadivoy": {"Director": "Sathish Vegesna", "genre": "All genres"},
+"Krishna Gaadi Veera Prema Gaadha": {"Director": "Sathish Vegesna", "genre": "All genres"},
+"Ninnu Kori": {"Director": "Sathish Vegesna", "genre": "All genres"},
+"Jersey": {"Director": "Sathish Vegesna", "genre": "All genres"},
+"MCA(Middle Class Madhu": {"Director": "Sathish Vegesna", "genre": "All genres"},
+"Anand": {"Director": "Shekar Kammula", "genre": "All genres"},
+"Godavari": {"Director": "Shekar Kammula", "genre": "All genres"},
+"Kotha BAngaru Lokam": {"Director": "Shekar Kammula", "genre": "All genres"},
+"Yeto Vellipoyindhi Manasu": {"Director": "Shekar Kammula", "genre": "All genres"},
+"Malli Malli Idi Rani Roju": {"Director": "Shekar Kammula", "genre": "All genres"},
+"Fidaa": {"Director": "Shekar Kammula", "genre": "All genres"},
+"Happy Days": {"Director": "Shekar Kammula", "genre": "All genres"},
+"Life is Beautiful": {"Director": "Shekar Kammula", "genre": "All genres"},
+"Leader": {"Director": "Shekar Kammula", "genre": "All genres"},
+"Oosaravelli": {"Director": "Surender Reddy", "genre": "All genres"},
+"Yamadonga": {"Director": "Surender Reddy", "genre": "All genres"},
+"Ashok": {"Director": "Surender Reddy", "genre": "All genres"},
+"Brindavanam": {"Director": "Surender Reddy", "genre": "All genres"},
+"Rakhi": {"Director": "Surender Reddy", "genre": "All genres"},
+"Gamyam":{"Director": "Krish", "genre": "All genres"},
+"Ashta Chamma": {"Director": "Krish", "genre": "All genres"},
+"Kanche": {"Director": "Krish", "genre": "All genres"},
+"Krishnam Vande Jagadgurum": {"Director": "Krish", "genre": "All genres"},
+"Vedam": {"Director": "Krish", "genre": "All genres"},
+"Manmadhudu": {"Director": "K. Vijaya Bhaskar", "genre": "All genres"},
+"Mass": {"Director": "K. Vijaya Bhaskar", "genre": "All genres"},
+"Ragada": {"Director": "K. Vijaya Bhaskar", "genre": "All genres"},
+"Surya vs Surya": {"Director": "K. Vijaya Bhaskar", "genre": "All genres"},
+"Manam": {"Director": "K. Vijaya Bhaskar", "genre": "All genres"},
+}
+
+# Function to recommend books and songs based on user preferences`
+# Function to recommend books, songs, and movies based on user preferences
+
+# Function to recommend books, songs, and movies based on user preferences
+# Function to recommend books, songs, and movies based on user preferences
+def recommend_items(preferences, choice):
+    recommended_items = []
+    if choice == 1:  # Books
+        for item, details in books_data.items():
+            if (details["Language"] in preferences or any(author.strip() in preferences for author in details["author"].split(',')) or item in preferences) and item not in recommended_items:
+                recommended_items.append(item)
+    elif choice == 2:  # Songs
+        for item, details in songs_data.items():
+            if (details["movie"] in preferences or details["artist"] in preferences or item in preferences) and item not in recommended_items:
+                recommended_items.append(item)
+    elif choice == 3:  # Movies
+        for key, details in movie_data.items():
+            movie_titles = key.split(",")
+            for movie_title in movie_titles:
+                if (details["genre"] in preferences or details["Director"] in preferences or movie_title.strip() in preferences) and movie_title.strip() not in recommended_items:
+                    recommended_items.append(movie_title.strip())
+    if not recommended_items:
+        print("Sorry, no items match your preferences.")
+    return recommended_items
+
+print("Welcome to the Recommendation System!")
+print("Please choose what you're interested in:")
+print("1. Books")
+print("2. Songs")
+print("3. Movies")
+
+choice = int(input("Enter your choice (1/2/3): "))
+
+if choice == 1:
+    print("Please enter your preferences for books. You can enter authors, languages, or specific titles.")
+elif choice == 2:
+    print("Please enter your preferences for songs. You can enter artists, movies, or specific song titles.")
+elif choice == 3:
+    print("Please enter your preferences for movies. You can enter genres, directors, or specific movie titles.")
+
+preferences = input("Enter your preferences (separated by commas): ").split(',')
+
+recommendations = recommend_items(preferences, choice)
+for recommendation in recommendations:
+    print(recommendation)
